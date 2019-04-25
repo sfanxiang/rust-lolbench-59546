fb198b0507b7a979d4cdf0420369fa49d2ea8e64
Xiang Fan <sfanxiang@gmail.com>
2019-04-25 12:14:49 +0800

    Benchmark

---
+++ run.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+
+##### Initialization #####
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+sudo cat /proc/cpuinfo
+sudo cat /proc/meminfo
+free
+sudo dmidecode
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain none
+export PATH="$PATH:$HOME/.cargo/bin"
+
+mkdir -p toolchain-none/{bin,lib}
+cp /bin/false toolchain-none/bin/rustc
+rustup toolchain link none toolchain-none
+rustup default none
+
+##### Set up Rust repo #####
+git clone https://github.com/rust-lang/rust.git
+cd rust
+
+git submodule init
+git submodule update src/doc/* src/stdsimd src/tools/*
+git submodule update --depth=1 # Fetching submodules is very slow. This can speed it up.
+
+$IN_TREE || git fetch origin $TARGET_COMMIT
+git reset --hard $TARGET_COMMIT
+git rebase $REBASE_COMMIT
+
+echo "[llvm]" >> config.toml
+echo "allow-old-toolchain = true" >> config.toml
+echo "[rust]" >> config.toml
+echo "codegen-units = 0" >> config.toml
+
+./x.py build src/tools/cargo --stage=1
+./x.py build --stage=1
+cp build/x86_64-unknown-linux-gnu/stage1-tools-bin/cargo build/x86_64-unknown-linux-gnu/stage1/bin/cargo
+
+cd ..
+
+##### Benchmark #####
+BENCHMARK_COMMIT=$TARGET_COMMIT bash bench.sh
